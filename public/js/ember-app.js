ba03e711e48d174651f74547476e7ed279fcb9f5
Stefan Hoth <sh@jnamic.com>
2011-12-22 17:08:28 +0100

    Basic version of homepage

--- public/js/ember-app.js
+++ public/js/ember-app.js
@@ -1,5 +1,21 @@
-var App = Em.Application.create();
+var ET = Ember.Application.create();
 
-App.Start = Em.View.extend({
-
+ET.User = Ember.Object.extend({
+  username: "stevie",
+  isLoggedIn: true
 });
+
+ET.UserStatus = Ember.View.extend({
+  templateName: 'UserStatus',
+  
+  username: ET.User.username,
+  isLoggedIn: ET.User.isLoggedIn
+});
+
+ET.Home = Ember.View.extend({
+  templateName: 'Home',
+  
+  username: "stevie",
+  isLoggedIn: true
+});
+
